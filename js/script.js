64af3e77c7a3b28b20605258a4214ec40530c1ec
tefcastineiras <54291296+tefcastineiras@users.noreply.github.com>
2023-06-20 19:05:20 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+
+function inclusive() {
+    document.body.style.backgroundColor = "black";
+    document.getElementById("main").style.backgroundColor = "black";
+    document.getElementById("main").style.color = "#ffffff";
+    document.getElementById("header").style.backgroundColor = "black";
+    document.getElementById("header").style.color = "#ffffff";
+    document.getElementById("rrss").style.backgroundColor = "black";
+    document.getElementById("nav").style.backgroundColor = "black";
+    document.getElementById("section").style.backgroundColor = "black";
+    document.getElementById("section").style.color = "#ffffff";
+    document.getElementById("aside").style.backgroundColor = "black";
+    document.getElementById("footer").style.backgroundColor = "black";
+
+
+}
+
+
+
+function Copyright() {
+    alert('Autora: Estefanía Castiñeiras');
+}
